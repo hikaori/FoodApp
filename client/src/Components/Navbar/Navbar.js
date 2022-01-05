8555cd2d88f456b9870ddf10f3be7fdf10b66b58
hikaori <kaori55194@gmail.com>
2022-01-05 22:31:07 +0900

    step3/37

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { AppBar, Button, Typography, Toolbar, Avatar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
 import useStyles from './styles';
 import memories from '../../images/memories.png';
 
 const Navbar = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
 
-  console.log('nabbar');
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+    navigate('/');
+    setUser(null);
+  };
 
   useEffect(() => {
     console.log('useEffect');
     const token = user?.token;
     setUser(JSON.parse(localStorage.getItem('profile')));
-  }, []);
+  }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -48,6 +57,7 @@ const Navbar = () => {
               variant="contained"
               className={classes.logout}
               color="secondary"
+              onClick={logout}
             >
               Logout
             </Button>
